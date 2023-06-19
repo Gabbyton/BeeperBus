4bde1f48aca70db5fb4d9f1ba137a542758320ef
Gabbyton <gponon2000@gmail.com>
2023-06-19 15:22:24 -0700

    added routing to arrival; added TODOs

--- data-collection-server/models/Arrival.js
+++ data-collection-server/models/Arrival.js
@@ -13,7 +13,20 @@ const Arrival = sequelize.define("Arrival", {
     type: DataTypes.DATE,
     allowNull: false,
   },
+  // TODO: model arrival data
   // Add other relevant fields here
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const data = await fetchTranslocData(); // Implement the logic to fetch data from the Transloc API
+    // Save the data to the database
+    const arrival = await Arrival.bulkCreate(data);
+    res.json(arrival);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = Arrival;
