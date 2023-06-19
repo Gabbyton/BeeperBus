6efc1bd41021127c7caf55e49dd3871d89a3f1de
Gabbyton <gponon2000@gmail.com>
2023-06-19 15:22:48 -0700

    added modelling to Vehicle; added todos;

--- data-collection-server/models/Vehicle.js
+++ data-collection-server/models/Vehicle.js
@@ -5,8 +5,26 @@ const router = express.Router();
 const Vehicle = require("../models/Vehicle");
 const fetchTranslocData = require("../utils/fetchTranslocData");
 
+const Vehicle = sequelize.define("Vehicle", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  time: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  // TODO: model vehicle data
+  // Add other relevant fields here
+  //
+  // TODO: add REST implementation
+  //
+});
+
 router.get("/", async (req, res) => {
   try {
+    // TODO: separate utilities for data types
     const data = await fetchTranslocData(); // Implement the logic to fetch data from the Transloc API
     // Save the data to the database
     const vehicles = await Vehicle.bulkCreate(data);
