bbcc65b0ca44f26b7b5884ec7c82acd62df3e1b9
Gabbyton <gponon2000@gmail.com>
2023-07-03 20:21:38 -0700

    reset to basic express server

--- data-collection-server/index.js
+++ data-collection-server/index.js
@@ -2,14 +2,12 @@
 
 const express = require("express");
 const app = express();
-const sequelize = require("./db");
-//
-// Create the database and tables if they don't exist
-sequelize.sync({ force: false }).then(() => {
-  console.log("Database and tables synced!");
-  // Start the server
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("this is working!");
+});
+
+app.listen(port, () => {
+  console.log("this is working!");
 });
