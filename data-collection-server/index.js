db94cd859f32b6b776261634e9fbbb74e6237607
Gabbyton <gponon2000@gmail.com>
2023-07-03 20:19:51 -0700

    removed non-functional models

--- data-collection-server/index.js
+++ data-collection-server/index.js
@@ -3,17 +3,7 @@
 const express = require("express");
 const app = express();
 const sequelize = require("./db");
-const Vehicle = require("./models/Vehicle");
-const Arrival = require("./models/Arrival");
-
-// Import route handlers
-const vehiclesRoute = require("./routes/vehicles");
-const arrivalsRoute = require("./routes/arrivals");
-
-// Define routes
-// app.use("/vehicles", vehiclesRoute);
-// app.use("/arrivals", arrivalsRoute);
-
+//
 // Create the database and tables if they don't exist
 sequelize.sync({ force: false }).then(() => {
   console.log("Database and tables synced!");
