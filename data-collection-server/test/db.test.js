3ea7ebd87c9bd6c401d77572bfdadf3796ebfeaf
Gabbyton <gponon2000@gmail.com>
2023-07-03 20:15:13 -0700

    removed redundant tests

--- data-collection-server/test/db.test.js
+++ data-collection-server/test/db.test.js
@@ -1,5 +1,63 @@
-const { describe, it } = require("mocha");
+// db.test.js
+// TODO: document test
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const { Sequelize } = require("sequelize");
 
-describe("express server starting", function () {
-  it("express server is starting", function () {});
+const config = require("../config");
+
+const env = process.env.NODE_ENV || "development";
+const dbConfig = config[env];
+
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+  }
+);
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe("Express Server Test", function () {
+  before(async function () {
+    await sequelize.authenticate();
+  });
+
+  // Check if the "beeperbus" database exists
+  it('should have the "beeperbus" database', async function () {
+    const query = `SELECT datname FROM pg_catalog.pg_database WHERE datname = 'beeperbus'`;
+    const result = await sequelize.query(query, {
+      type: sequelize.QueryTypes.SELECT,
+    });
+
+    expect(result.length).to.equal(1);
+  });
+
+  // Check if the "arrivals" table exists
+  it('should have the "arrivals" table', async function () {
+    const query = `SELECT to_regclass('beeperbus.arrivals')`;
+    const result = await sequelize.query(query, {
+      type: sequelize.QueryTypes.SELECT,
+    });
+
+    expect(result[0].to_regclass).to.equal("beeperbus.arrivals");
+  });
+
+  // Check if the "vehicles" table exists
+  it('should have the "vehicles" table', async function () {
+    const query = `SELECT to_regclass('beeperbus.vehicles')`;
+    const result = await sequelize.query(query, {
+      type: sequelize.QueryTypes.SELECT,
+    });
+
+    expect(result[0].to_regclass).to.equal("beeperbus.vehicles");
+  });
+
+  after(async function () {
+    await sequelize.close();
+  });
 });
--- data-collection-server/test/index.test.js
+++ data-collection-server/test/index.test.js
@@ -4,27 +4,21 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { describe, it } = require("mocha");
-const app = require("../index");
-
-chai.use(chaiHttp);
 
 const expect = chai.expect;
-const assert = chai.assert;
+chai.use(chaiHttp);
 
-describe("mocha runs", function () {
-  it("must be equal", function (done) {
-    assert.equal(1, 1);
-    done();
-  });
-});
-
-describe("express server", function () {
-  it("runs", function () {
-    chai
-      .request(app)
-      .get("/")
-      .end(function (_, res) {
+// Replace 'http://localhost:3000' with the actual server URL
+chai
+  .request("http://localhost:3000")
+  .get("/")
+  .end((err, res) => {
+    describe("Server", () => {
+      it("should have a successful status code", () => {
         expect(res).to.have.status(200);
       });
+    });
   });
-});
+
+// Run the tests
+mocha.run();
