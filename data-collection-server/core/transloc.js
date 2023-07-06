8fd45c3560ab375f09a15d39b26dca02dd855862
Joshua Shew <joshua.t.shew@gmail.com>
2023-07-06 17:06:06 -0400

    Updated transloc.js Changed options to vehicleOptions Added arrivalOptions with sample API call Added getArrivals function

--- data-collection-server/core/transloc.js
+++ data-collection-server/core/transloc.js
@@ -5,7 +5,7 @@ const config = require("../secrets/config");
 const env = process.env.NODE_ENV || "development";
 const apiConfig = config[env];
 
-const options = {
+const vehicleOptions = {
   method: "GET",
   url: "https://transloc-api-1-2.p.rapidapi.com/vehicles.json",
   params: {
@@ -18,9 +18,24 @@ const options = {
   },
 };
 
+const arrivalOptions = {
+  method: "GET",
+  url: "https://transloc-api-1-2.p.rapidapi.com/arrival-estimates.json",
+  params: {
+    agencies: "1199",
+    routes: '4000421,4000592,4005122',  // TODO add actual routes and stops
+    stops: '4002123,4023414,4021521',
+    callback: "call",
+  },
+  headers: {
+    "X-RapidAPI-Key": apiConfig.apikey,
+    "X-RapidAPI-Host": apiConfig.apihost,
+  },
+}
+
 getVehicles = async function () {
   try {
-    const response = await axios.request(options);
+    const response = await axios.request(vehicleOptions);
     rawVehicleData = response.data["data"]["1199"];
     return rawVehicleData;
   } catch (error) {
@@ -29,3 +44,15 @@ getVehicles = async function () {
 };
 
 getVehicles();
+
+getArrivals = async function () {
+  try {
+    const response = await axios.request(arrivalOptions);
+    const rawArrivalsData = response.data["data"]
+    return rawArrivalsData;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getArrivals();
