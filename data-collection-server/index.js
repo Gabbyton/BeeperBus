6491644ed2686ac4fcbeaa4705bf16815c7687ba
Gabbyton <gponon2000@gmail.com>
2023-07-08 13:36:35 -0700

    added chron schedule to main

--- data-collection-server/index.js
+++ data-collection-server/index.js
@@ -1,6 +1,11 @@
 const app = require("./core/app");
 const port = 3000;
 
+const { scheduleCollect } = require("./core/collect");
+
+console.log("starting data collection operation...");
+scheduleCollect();
+
 app.listen(port, function () {
   console.log(`server listening at port ${port}...`);
 });
