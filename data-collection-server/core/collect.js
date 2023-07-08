573fd5a4ad4bc18e709f2505f2c22d3a61a63cc1
Gabbyton <gponon2000@gmail.com>
2023-07-08 13:36:14 -0700

    added logging and cron job scheduling

--- data-collection-server/core/collect.js
+++ data-collection-server/core/collect.js
@@ -3,24 +3,22 @@ const path = require("path");
 
 const cron = require("node-cron");
 
-const Vehicle = require("../models/vehicle")
-const { getVehicles } = require("transloc")
+const Vehicle = require("../models/vehicle");
+const { getVehicles } = require("./transloc");
 
 // Create a log file for error logging
-const errlogFilePath = path.join(__dirname, "error.log");
-const savelogFilePath = path.join(__dirname, "save.log");
+const parentPath = path.resolve(__dirname, "..");
+const errlogFilePath = path.join(parentPath, "logs", "app-error.log");
+const savelogFilePath = path.join(parentPath, "logs", "save-log.log");
 const errorLogStream = fs.createWriteStream(errlogFilePath, { flags: "a" });
 const saveLogStream = fs.createWriteStream(savelogFilePath, { flags: "a" });
-
 
 // Function to collect and save data
 async function collectAndSaveData() {
   try {
     const vehicleData = await getVehicles();
-    await Vehicle.serialize(vehicleData, doSave = true);
-    saveLogStream.write(
-      `[${new Date().toISOString()}]\tSaved vehicle data\n`
-    );
+    await Vehicle.serialize(vehicleData, (doSave = true));
+    saveLogStream.write(`[${new Date().toISOString()}]\tSaved vehicle data\n`);
   } catch (err) {
     errorLogStream.write(
       `[${new Date().toISOString()}]\tError collecting vehicle data:\n${err}\n`
@@ -28,5 +26,10 @@ async function collectAndSaveData() {
   }
 }
 
-// Schedule the data collection using cron (every second)
-cron.schedule("* * * * * *", collectAndSaveData);
+scheduleCollect = function () {
+  // Schedule the data collection using cron (every second
+  const cronJob = cron.schedule("* * * * * *", collectAndSaveData);
+  return cronJob;
+};
+
+module.exports = { scheduleCollect };
