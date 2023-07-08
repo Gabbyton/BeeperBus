df2a10298fa84b974acc71f2635a28aa234f855a
Gabbyton <gponon2000@gmail.com>
2023-07-08 12:12:16 -0700

    added error log middleware

--- data-collection-server/core/app.js
+++ data-collection-server/core/app.js
@@ -1,5 +1,18 @@
+const path = require("path");
+const fs = require("fs");
+
 const express = require("express");
 const app = express();
+
+const errlogFilePath = path.join(__dirname, "app-error.log");
+const errorLogStream = fs.createWriteStream(errlogFilePath, { flags: "a" });
+
+// Middleware for logging errors
+app.use((err, req, res, next) => {
+  console.error(err);
+  errorLogStream.write(`${new Date().toISOString()} - ${err.stack}\n`);
+  next(err);
+});
 
 app.get("/", (req, res) => {
   res.send("this is working!");
