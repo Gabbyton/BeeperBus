4570280b58fb2aad1a8ee370efea60c851a418bc
Joshua Shew <joshua.t.shew@gmail.com>
2023-07-10 19:58:49 -0400

    Updated transloc.test.js Added tests for arrival serialization Moved arrival tests together

--- data-collection-server/test/transloc.test.js
+++ data-collection-server/test/transloc.test.js
@@ -2,7 +2,7 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { describe, it } = require("mocha");
 
-const { getVehicles } = require("../core/transloc");
+const { getVehicles, getArrivals } = require("../core/transloc");
 
 const Vehicle = require("../models/vehicle");
 const { scheduleCollect } = require("../core/collect");
@@ -17,19 +17,13 @@ describe("Transloc Open API", function () {
     assert.isArray(data);
   });
 
-  it("responds to arrival queries", async function () {
-    const data = await getArrivals()
-    assert.isDefined(data);
-    assert.isArray(data["data"])
-  });
-
   it("returns a serialized vehicle", async function () {
     const data = await getVehicles();
     const [vehicleEntries, badEntries] = Vehicle.serialize(data);
-
+    
     assert.isArray(vehicleEntries);
     assert.isArray(badEntries);
-
+    
     if (vehicleEntries.length > 0) {
       vehicleEntries.forEach(function (entry) {
         assert.isObject(entry);
@@ -41,6 +35,34 @@ describe("Transloc Open API", function () {
       badEntries.forEach(function (entry) {
         assert.isObject(entry);
         console.warn(`encountered serialize error at object: ${entry}`);
+      });
+    }
+  });
+
+  it("responds to arrival queries", async function () {
+    const data = await getArrivals();
+    assert.isDefined(data);
+    assert.isArray(data["data"]);
+  });
+
+  it("returns a serialized arrival", async function () {
+    const rawArrivalsData = await getArrivals();
+    const [arrivalEntries, badEntries] = Arrival.serialize(rawArrivalsData);
+
+    assert.isArray(arrivalEntries);
+    assert.isArray(badEntries);
+
+    if (arrivalEntries.length) {
+      arrivalEntries.forEach(function (arrival) {
+        assert.isObject(arrival)
+      });
+    } else {
+      console.warn("serialization function did not return arrival objects");
+    }
+    if (badEntries.length) {
+      badEntries.forEach(function (badEntry) {
+        assert.isObject(badEntry);
+        console.warn(`encountered serialization error at object: ${badEntry}`);
       });
     }
   });
