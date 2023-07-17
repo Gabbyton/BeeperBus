5989363a8a6b4f92389fc51b5387a9e67c393f0b
Joshua Shew <joshua.t.shew@gmail.com>
2023-07-17 16:01:46 -0400

    Updated crud.test.js Added crud tests for Arrival model

--- data-collection-server/test/crud.test.js
+++ data-collection-server/test/crud.test.js
@@ -6,12 +6,13 @@ const { describe, it } = require("mocha");
 const db = require("../core/db");
 
 const Vehicle = require("../models/vehicle");
+const Arrival = require("../models/arrival");
 
 const expect = chai.expect;
 const assert = chai.assert;
 chai.use(chaiHttp);
 
-describe("database", function () {
+describe("database: vehicle", function () {
   it("connects to the database", function () {
     return db.authenticate();
   });
@@ -80,6 +81,91 @@ describe("database", function () {
     });
 
     const { count, _ } = await Vehicle.findAndCountAll({});
+    assert.equal(count, 1);
+  });
+});
+
+describe("database: arrival", function () {
+  it("connects to the database", function () {
+    return db.authenticate();
+  });
+
+  it("syncs the table and creates a new arrival entry", async function () {
+    await Arrival.sync({ force: true });
+
+    const generatedOnValues = {
+      originalValues: ["original-value-1", "original-value-2"],
+      updateValue: "updated-value-1",
+    };
+
+    await Arrival.create({
+      generatedOn: generatedOnValues.originalValues[0],
+      agencyId: "def456",
+      stopId: "xyz789",
+      routeId: 4567,
+      vehicleId: 1234,
+      arrivalAt: "tuv123",
+      type: "typeA",
+    });
+
+    await Arrival.create({
+      generatedOn: generatedOnValues.originalValues[1],
+      agencyId: "def456",
+      stopId: "xyz789",
+      routeId: 4567,
+      vehicleId: 1234,
+      arrivalAt: "tuv123",
+      type: "typeA",
+    });
+  });
+
+  it("retrieves an entry from the database", async function () {
+    const entries = await Arrival.findAll({
+      attributes: ["generatedOn"],
+    });
+
+    const retrievedGeneratedOnValues = [];
+    entries.forEach(function (entry) {
+      retrievedGeneratedOnValues.push(entry.generatedOn);
+    });
+
+    expect(retrievedGeneratedOnValues).to.have.members(
+      generatedOnValues.originalValues
+    );
+  });
+
+  it("can update an item", async function () {
+    await Arrival.update(
+      { generatedOn: generatedOnValues.updateValue },
+      {
+        where: {
+          generatedOn: generatedOnValues.originalValues[1],
+        },
+      }
+    );
+    const entries = await Arrival.findAll({
+      attributes: ["generatedOn"],
+    });
+
+    const retrievedGeneratedOnValues = [];
+    entries.forEach(function (entry) {
+      retrievedGeneratedOnValues.push(entry.generatedOn);
+    });
+
+    expect(retrievedGeneratedOnValues).to.have.members([
+      generatedOnValues.originalValues[0],
+      generatedOnValues.updateValue,
+    ]);
+  });
+
+  it("can delete an item", async function () {
+    await Arrival.destroy({
+      where: {
+        generatedOn: generatedOnValues.updateValue,
+      },
+    });
+
+    const { count, _ } = await Arrival.findAndCountAll({});
     assert.equal(count, 1);
   });
 });
