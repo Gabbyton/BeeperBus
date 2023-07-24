928b533e4c052bcdb954ad2acd5dfcfb92af9476
Joshua Shew <joshua.t.shew@gmail.com>
2023-07-24 14:09:40 -0400

    Added virtual environment config scripts

---
+++ data-analysis/scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Virtual Environment
+bash scripts/reinstall-venv.sh
+source .venv/bin/activate
+
+# Jupyter kernel
+ipython kernel install --user --name=zoll
+
+# Git
+git config filter.strip-notebook-output-metadata.clean "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --ClearMetadataPreprocessor.preserve_nb_metadata_mask kernelspec --ClearMetadataPreprocessor.preserve_nb_metadata_mask name --to=notebook --stdin --stdout --log-level=ERROR"
+git config filter.strip-notebook-output-metadata.smudge "cat"
