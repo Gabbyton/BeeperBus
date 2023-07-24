a685827a03edbb56c2caaafa1002c1fe962d44fc
Joshua Shew <joshua.t.shew@gmail.com>
2023-07-24 14:17:59 -0400

    Fixed kernel name

--- data-analysis/scripts/install.sh
+++ data-analysis/scripts/install.sh
@@ -5,7 +5,7 @@ bash scripts/reinstall-venv.sh
 source .venv/bin/activate
 
 # Jupyter kernel
-ipython kernel install --user --name=zoll
+ipython kernel install --user --name=data-analysis
 
 # Git
 git config filter.strip-notebook-output-metadata.clean "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --ClearMetadataPreprocessor.preserve_nb_metadata_mask kernelspec --ClearMetadataPreprocessor.preserve_nb_metadata_mask name --to=notebook --stdin --stdout --log-level=ERROR"
