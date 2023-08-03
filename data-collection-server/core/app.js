52662f78b88037859a20c3c27725480873a70c8e
Joshua Shew <joshua.t.shew@gmail.com>
2023-08-04 08:58:27 +0900

    Added winston logging to app.js

--- data-collection-server/core/app.js
+++ data-collection-server/core/app.js
@@ -1,17 +1,30 @@
 const path = require("path");
 const fs = require("fs");
 
+const winston = require('winston');
+const { format } = winston;
+const { combine, timestamp, label, json } = format;
+
 const express = require("express");
 const app = express();
 
-const parentPath = path.resolve(__dirname, "..");
-const errlogFilePath = path.join(parentPath, "logs", "app-error.log");
-const errorLogStream = fs.createWriteStream(errlogFilePath, { flags: "a" });
+winston.loggers.add('main', {
+  format: combine(
+    label({ label: 'main' }),
+    timestamp(),
+    json()
+  ),
+  transports: [
+    new winston.transports.Console({ level: 'silly'}),
+    new winston.transports.File({ filename: 'logs/combined.log', level: 'info' }),
+    new winston.transports.File({ filename: 'logs/errors.log', level: 'error' })
+  ]
+})
+const logger = winston.loggers.get('main');
 
 // Middleware for logging errors
 app.use((err, req, res, next) => {
-  console.error(err);
-  errorLogStream.write(`${new Date().toISOString()} - ${err.stack}\n`);
+  logger.error(err.message)
   next(err);
 });
 
