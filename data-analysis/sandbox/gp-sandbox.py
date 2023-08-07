118ed678cf600df33dc890290fcedcb4af028780
Joshua Shew <joshua.t.shew@gmail.com>
2023-08-08 05:55:24 +0900

    Formatted gp-sandbox.py

--- data-analysis/sandbox/gp-sandbox.py
+++ data-analysis/sandbox/gp-sandbox.py
@@ -1,13 +1,14 @@
-import os
+"""Placeholder docstring"""  # TODO: add docstring
+
 import json
 from itertools import chain
 
 import pandas as pd
-import numpy as np
 
-import matplotlib.pyplot as plt
 
 class Vehicle:
+    """Placeholder docstring"""  # TODO: add docstring
+
     route_name: str
     encoded_polyline: str
     lat: float
@@ -16,17 +17,19 @@ class Vehicle:
     stops: list
     route_id: int
     # TODO: add vehicle ID
-    
-    def __init__(self, data):
-        self.route_name = data["Description"]
-        self.encoded_polyline = data["EncodedPolyline"]
-        self.lat = data["MapLatitude"]
-        self.long = data["MapLongitude"]
-        self.color = data["MapLineColor"]
-        self.stops = data["Stops"]
-        self.route_id = data["RouteID"]
+
+    def __init__(self, input_data) -> None:
+        self.route_name = input_data["Description"]
+        self.encoded_polyline = input_data["EncodedPolyline"]
+        self.lat = input_data["MapLatitude"]
+        self.long = input_data["MapLongitude"]
+        self.color = input_data["MapLineColor"]
+        self.stops = input_data["Stops"]
+        self.route_id = input_data["RouteID"]
+
 
 class Stop:
+    """Placeholder docstring"""  # TODO: add docstring
 
     name: str
     line: str
@@ -37,33 +40,36 @@ class Stop:
     route_stop_id: int
     next_stop_est: int
 
-    def __init__(self, data):
-        self.name = data["SignVerbiage"]
-        self.line = data["Line1"]
-        self.lat = data["Latitude"]
-        self.long = data["Longitude"]
-        self.heading = data["Heading"]
-        self.route_id = data["RouteID"]
-        self.route_stop_id = data["RouteStopID"]
-        self.next_stop_est = data["SecondsToNextStop"]
+    def __init__(self, input_data) -> None:
+        self.name = input_data["SignVerbiage"]
+        self.line = input_data["Line1"]
+        self.lat = input_data["Latitude"]
+        self.long = input_data["Longitude"]
+        self.heading = input_data["Heading"]
+        self.route_id = input_data["RouteID"]
+        self.route_stop_id = input_data["RouteStopID"]
+        self.next_stop_est = input_data["SecondsToNextStop"]
 
-if __name__ == "__main__":
 
-    with open('data/shuttle_stops_with_routes.txt', 'r') as file:
+def main() -> None:
+    with open("data/shuttle_stops_with_routes.txt", "r", encoding="utf-8") as file:
         data = json.load(file)
 
     vehicles = list(map(Vehicle, data))
 
     for vehicle in vehicles:
-        vehicle.stops = list(map(Stop, vehicle.stops)) 
+        vehicle.stops = list(map(Stop, vehicle.stops))
 
     vehicle_data = list(map(vars, vehicles))
     all_stops = [vehicle.stops for vehicle in vehicles]
     all_stops = list(map(vars, chain(*all_stops)))
 
-    vehicle_df = pd.DataFrame(vehicle_data)
-    vehicle_df = vehicle_df.drop('stops', axis="columns")
-    stop_df =pd.DataFrame(all_stops)
+    vehicle_df = pd.DataFrame(vehicle_data).drop("stops", axis="columns")
+    stop_df = pd.DataFrame(all_stops)
 
     vehicle_df.to_csv("data/vehicle_df.csv")
     stop_df.to_csv("data/stop_df.csv")
+
+
+if __name__ == "__main__":
+    main()
